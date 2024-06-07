3475422736f73f66672728711f9d812e61ba62f3
dingc2 <cheng.ding@vanderbilt.edu>
2024-06-07 12:40:17 -0400

    added buttons that changes text

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve the HTML file
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
