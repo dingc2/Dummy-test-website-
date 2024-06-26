9249ed3ceebb936a8e7e8b44e801af581ed771a9
dingc2 <cheng.ding@vanderbilt.edu>
2024-06-26 16:03:39 -0400

    added cypress and test scripts

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,24 @@
+describe('Dummy website home page', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+  it('h1 contains the correct text', () => {
+    cy.get("h1").should("exist").contains("My Dummy Website");
+  });
+  it('check to make sure list is correct', () => {
+    cy.get("ul").should("exist").contains("Google Link");
+    cy.get("ul").should("exist").contains("Bing Link");
+    cy.get("ul").should("exist").contains("Duckduckgo Link");
+    cy.get("ul").should("exist").contains("Goes Nowhere");
+
+  });
+  it('test button change', () => {
+    cy.get('.btn-primary').should("exist").contains("Clicking this button will change the text").click();
+    cy.get('#testParagraph').should("exist").contains("THIS IS CHANGED");
+    cy.get('.box-1 > button').should("exist").contains("Press me").click();
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('I hate JavaScript');
+    });
+    cy.get('.btn-2').should("exist").contains("Press me").click();
+  });
+})
--- myScript.js
+++ myScript.js
@@ -1,3 +1,12 @@
 function changeText(id, changeString) {
     document.getElementById(id).innerHTML = changeString;
 }
+
+const button2 = document.querySelector('.btn-2');
+function alertButton() {
+    alert('You hate javascript too!');
+}
+// button2.addEventListener("click", () => {
+//     alert('you hate javascript');
+// });
+button2.addEventListener("click", alertButton);
