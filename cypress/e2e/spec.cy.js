ef40f418ca24ed97410a9ffebf496b90fd26d9ff
dingc2 <cheng.ding@vanderbilt.edu>
2024-06-27 10:07:29 -0400

    fixed error in html and spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -15,10 +15,15 @@ describe('Dummy website home page', () => {
   it('test button change', () => {
     cy.get('.btn-primary').should("exist").contains("Clicking this button will change the text").click();
     cy.get('#testParagraph').should("exist").contains("THIS IS CHANGED");
-    cy.get('.box-1 > button').should("exist").contains("Press me").click();
+    cy.get('.box-1 > button').should("exist").contains("Press me").click()
     cy.on('window:alert', (alertText) => {
       expect(alertText).to.equal('I hate JavaScript');
     });
-    cy.get('.btn-2').should("exist").contains("Press me").click();
   });
+  it('test second button', () => {
+    cy.get('.btn-2').should("exist").contains("Press me!!!").click()
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('You hate javascript too!');
+    });
+  })
 })
